397a3d4cee2812d54857fb546cff627e96889849
Bruno Rocha <brunorocha2674@gmail.com>
2022-04-06 15:59:08 -0300

    test: add LogRepository

---
+++ __tests__/infra/mongodb/LogRepository.spec.ts
@@ -0,0 +1,29 @@
+import { LogRepository, MongoHelper } from '@/infra/mongodb'
+import { Collection } from 'mongodb'
+let logsCollection: Collection
+
+const makeSut = (): LogRepository => {
+  return new LogRepository()
+}
+
+describe('LogRepository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+    logsCollection = await MongoHelper.getCollection('logs')
+  })
+
+  beforeEach(async () => {
+    await logsCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  it('should register error on success', async () => {
+    const sut = makeSut()
+    await sut.log('any-stack')
+    const count = await logsCollection.countDocuments()
+    expect(count).toBe(1)
+  })
+})
